414bcaf400707a59cca7683bceafbaba64b36316
vishalkgit <kalevishal1012@gmail.com>
2024-12-08 19:18:32 +0530

    Add project files.

---
+++ Bank.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//code for Bank with Debit and Credit using Event And Delegates
+namespace Event_Delegates
+{
+
+    
+
+   
+    public class Bank
+    {
+        public event DisplayMessage GreaterAmount;
+        public event DisplayMessage LowBalance;
+        public event DisplayMessage ZeroBalance;
+
+        private float balance;
+        private float amount;
+
+        public Bank(float balance)
+        {
+            this.balance = balance;
+        }
+
+        public Bank()
+        {
+        }
+
+        public void Debit(float amount)
+        {
+            if(balance==0)
+            {
+                ZeroBalance();
+            }
+            else if(balance<=3000)
+            {
+                LowBalance();
+            }
+            else if(amount>balance)
+            {
+                GreaterAmount();
+            }
+            else
+            {
+                balance = balance - amount;
+                Console.WriteLine($"Balance after debiting amount{amount} is {balance}");
+            }
+            
+        }
+
+        public void Credit(float amount)
+        {
+            balance=balance+amount;
+            Console.WriteLine($"Balance after crediting {amount} is {balance}");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Delegates
+{
+    public delegate void GreaterAmount();
+    public delegate void LowBalance();
+    public delegate void ZeroBalance();
+
+    public delegate void DisplayMessage();
+    public class Program
+    {
+
+        static void GreaterAmount()
+        {
+            Console.WriteLine("Amount is greater than balance, cant deduct money");
+        }
+
+        static void LowBalance()
+        {
+            Console.WriteLine("Balance is less than 3000, cant deduct money");
+        }
+
+        static void ZeroBalance()
+        {
+            Console.WriteLine("Your account balance is zero");
+        }
+        static void Main(string[] args)
+        {
+            Bank bk=new Bank(5000);
+            bk.GreaterAmount += new DisplayMessage(GreaterAmount);
+            bk.LowBalance += new DisplayMessage(LowBalance);
+            bk.ZeroBalance += new DisplayMessage(ZeroBalance);
+
+            bk.Debit(6000);
+            //bk.Credit(15000);
+
+
+
+        }
+    }
+}
